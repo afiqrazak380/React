f00b6c5f537393240f830b7cd75dd2d5692f2594
AfiqRazak <afiqrazak380@gmail.com>
2023-11-15 22:46:15 +0000

    Add comments

--- A controlled form.jsx
+++ A controlled form.jsx
@@ -6,10 +6,10 @@ class MyForm extends React.Component {
     super(props);
 
     // Set the initial state of the component
-    this.state = {
+    this.state({
       input: "", // Initialize the input property in state with an empty string
       submit: "", // Initialize the submit property in state with an empty string
-    };
+    });
 
     // Bind the context of the handleChange and handleSubmit methods to the current instance
     this.handleChange = this.handleChange.bind(this);
@@ -37,16 +37,18 @@ class MyForm extends React.Component {
 
   // Render method that defines the component's UI
   render() {
+    // Return a div containing a form and an h1 tag
     return (
-      // Return a div containing a form and an h1 tag
       <div>
-        {/* Form element with a submit event handler */}
+        {/* Form element with a submit event handler*/}
         <form onSubmit={this.handleSubmit}>
           {/* Input element with value and onChange attributes */}
           <input value={this.state.input} onChange={this.handleChange} />
+
           {/* Button to submit the form */}
           <button type="submit">Submit!</button>
         </form>
+
         {/* Heading element displaying the value of the submit property from the component's state */}
         <h1>{this.state.submit}</h1>
       </div>
